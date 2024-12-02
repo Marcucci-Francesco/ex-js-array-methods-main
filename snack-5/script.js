d5f6e7893b36bd9e3187b2e5edee090cb70451c2
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-12-02 17:42:03 +0100

    feat: add changes snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -3,11 +3,8 @@ const nums = [2, 8, 4, 7, 12, 87];
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
 
-const pari = [];
-for (let num of nums){
-  if (!(num % 2)){
-    pari.push(num);
-  }
-}
+const pari = nums.filter(num => {
+  return !(num % 2)
+})
 
 console.log(pari);
