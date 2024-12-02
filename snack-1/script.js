75839834081a5cb0279486c198d31a25251eab12
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-12-02 13:28:43 +0100

    feat: snack1 completed

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const nomi = names.map((name) => `'${name}'`);
+const name = nomi.join(',');
+
+console.log(name);
